b448b37cca4b07e5ea94bea8b85a28c8e2280b2e
shawnjiang1019 <64927869+shawnjiang1019@users.noreply.github.com>
2021-07-11 14:31:56 -0600

    Update docusaurus.config.js
    Changed parts of the navbar to add the python page and change the original header to say DataEraIt Association

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,18 +3,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'DataEraIt Association Project',
+  tagline: 'Some tutorials',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'DataEraIt Association Project', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'DataEraIt Association',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -26,7 +26,8 @@ module.exports = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        
+        {to: '/python', label: 'Python', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -107,3 +108,4 @@ module.exports = {
     ],
   ],
 };
+
