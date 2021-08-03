df071481f3a7518a1136c50688ad169c0720bbb1
George Zhang <george.jx.zhang@gmail.com>
2021-08-03 09:12:05 -0600

    Revert "added more changes"
    This reverts commit a8415f0f7064009807713afcf546361ea3a71259.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,45 @@
 import React from 'react';
+import 'normalize.css';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import SnakeGame from '../components/SnakeGame';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Learn Coding with simple excercises ⏱️
-          </Link>
+    <header className={ clsx( 'hero hero--primary', styles.heroBanner ) }>
+      <div className="container" >
+        <div className="row">
+          <div className="col col--6">
+            <h1 className="hero__title">{ siteConfig.title }</h1>
+            <p className="hero__subtitle">{ siteConfig.tagline }</p>
+            <div className={ styles.buttons }>
+              <Link
+                className="button button--secondary button--lg"
+                to="/docs/intro">
+                Learn Coding with simple excercises ⏱️
+              </Link>
+            </div>
+          </div>
+          <div className="col col--6">
+            <SnakeGame />
+          </div>
         </div>
       </div>
+
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={ `Hello from ${ siteConfig.title }` }
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
